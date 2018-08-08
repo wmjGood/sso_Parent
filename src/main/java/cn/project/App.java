68a61c976ae21c39420da2804951575868bccebf
wmjGood <2116743767@qq.com>
2018-08-08 15:11:21 +0800

    Initial commit

---
+++ src/main/java/cn/project/App.java
@@ -0,0 +1,13 @@
+package cn.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
